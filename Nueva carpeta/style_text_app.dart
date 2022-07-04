4aa1379f1c348559142c4b24ab9cd27fea2120dd
Chapista <108679509+Chapista@users.noreply.github.com>
2022-07-04 09:32:01 -0500

    Archivos html
    donde se aplican algunas propiedades de css

---
+++ Nueva carpeta/style_text_app.dart
@@ -0,0 +1,70 @@
+part of '/values/theme_app.dart';
+
+TextStyle _styleDMSans(FontWeight weight)=> TextStyle(
+	fontFamily: "DMSans-Regular",
+	fontWeight: weight
+);
+
+TextStyle _styleIbmPlexSans(FontWeight weight)=> TextStyle(
+	fontFamily: "IBMPlexSans-Regular",
+	fontweight: weight
+);
+
+
+TextStyle get _styleIbmPlexSans600 =>_styleIbmPlexSans(FontWeight.w600);
+TextStyle get _styleIbmPlexSans600 =>_styleIbmPlexSans(FontWeight.w700);
+
+TextStyle get _styleIbmPlexSans600 =>_styleIbmPlexSans(FontWeight.w400);
+TextStyle get _styleIbmPlexSans600 =>_styleIbmPlexSans(FontWeight.w700);
+
+TextStyle get _styleh1 => TextStyle(
+	fontSize: sizeTexth1	
+);
+
+TextStyle get _styleh2 => TextStyle(
+	fontSize: sizeTexth2	
+);
+
+TextStyle get _styleh3 => TextStyle(
+	fontSize: sizeTexth3	
+);
+
+TextStyle get _styleh4 => TextStyle(
+	fontSize: sizeTexth4	
+);
+
+TextStyle get _styleh5 => TextStyle(
+	fontSize: sizeTexth5	
+);
+
+TextStyle get _styleh6 => TextStyle(
+	fontSize: sizeTexth6	
+);
+
+TextStyle get _styleSubtitle1 => TextStyle(
+	fontSize: sizeTextSubtitle1
+);
+
+TextStyle get _styleSubtitle2 => TextStyle(
+	fontSize: sizeTextSubtitle2
+);
+
+TextStyle get _styleBodyText1 => TextStyle(
+	fontSize: sizeTextBodyText1
+);
+
+TextStyle get _styleBodyText2 => TextStyle(
+	fontSize: sizeTextBodyText2
+);
+
+TextStyle get _styleButton => TextStyle(
+	fontSizeTextButton
+);
+
+TextStyle get _styleCation => TextStyle(
+	fontSize: sizeTextCaption
+);
+
+TextStyle get _styleOverLine => TextStyle(
+	fontSize: sizeTextOverLine
+);
